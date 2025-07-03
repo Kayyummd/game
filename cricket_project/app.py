f365440f731a4f4f801ea051d3b8ef486885e991
Kayyummd <kayyummd.h@gmail.com>
2025-07-03 23:42:47 +0530

    Update app.py

--- cricket_project/app.py
+++ cricket_project/app.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os, random
 
-# --- Flask Setup ---
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = 'super_secret_key'
 CORS(app)
@@ -78,7 +77,7 @@ def room():
 def multiplayer_game():
     return render_template("multiplayer_game.html")
 
-# --- Game State Management ---
+# --- Game State ---
 rooms = {}
 
 @socketio.on("join_room")
@@ -169,33 +168,64 @@ def handle_player_move(data):
         m1, m2 = r["moves"][p1], r["moves"][p2]
 
         batter_sid = r["bat_first_sid"] if r["innings"] == 1 else next(p for p in r["players"] if p != r["bat_first_sid"])
-        r["scores"][batter_sid] = r["scores"].get(batter_sid, 0) + (m1 if batter_sid == p1 else m2)
 
-     elif m1 == m2 and r["innings"] == 2:
-         winner = p2 if r["scores"][p2] > r["scores"][p1] else p1
-         r["target"] = None
-         emit("round_result", {...}, room=room)
-         emit("game_over", {
-        "winner_sid": winner,
-        "winner_name": r["names"][winner]
-         }, room=room)
-
-            else:
-                p1_score = r["scores"].get(p1, 0)
-                p2_score = r["scores"].get(p2, 0)
-                if p1_score > p2_score:
-                    winner, loser = p1, p2
-                elif p2_score > p1_score:
-                    winner, loser = p2, p1
-                else:
-                    winner = loser = None
-                emit("game_over", {
-                    "winner": winner,
-                    "loser": loser,
-                    "winner_name": r["names"].get(winner, "Player") if winner else "Tie",
-                    "loser_name": r["names"].get(loser, "Player") if loser else "Tie"
+        # OUT condition
+        if m1 == m2:
+            if r["innings"] == 1:
+                r["target"] = r["scores"].get(batter_sid, 0) + 1
+                r["innings"] = 2
+                emit("round_result", {
+                    "type": "out",
+                    "num": m1,
+                    "moves": {p1: m1, p2: m2},
+                    "usernames": r["names"]
                 }, room=room)
+            else:
+                emit("round_result", {
+                    "type": "out",
+                    "num": m1,
+                    "moves": {p1: m1, p2: m2},
+                    "usernames": r["names"]
+                }, room=room)
+                scores = r["scores"]
+                p1_score = scores.get(p1, 0)
+                p2_score = scores.get(p2, 0)
+                if p1_score > p2_score:
+                    winner = p1
+                elif p2_score > p1_score:
+                    winner = p2
+                else:
+                    winner = None  # Tie
+
+                emit("game_over", {
+                    "winner_sid": winner,
+                    "winner_name": r["names"].get(winner, "Tie") if winner else "Tie"
+                }, room=room)
+                r["target"] = None
+
         else:
+            if batter_sid == p1:
+                r["scores"][p1] = r["scores"].get(p1, 0) + m1
+            else:
+                r["scores"][p2] = r["scores"].get(p2, 0) + m2
+
+            if r["innings"] == 2 and r["target"] is not None:
+                if r["scores"].get(batter_sid, 0) >= r["target"]:
+                    emit("round_result", {
+                        "type": "continue",
+                        "moves": {p1: m1, p2: m2},
+                        "usernames": r["names"],
+                        "target": r["target"],
+                        "scores": r["scores"]
+                    }, room=room)
+                    emit("game_over", {
+                        "winner_sid": batter_sid,
+                        "winner_name": r["names"].get(batter_sid, "Player")
+                    }, room=room)
+                    r["target"] = None
+                    r["moves"] = {}
+                    return
+
             emit("round_result", {
                 "type": "continue",
                 "moves": {p1: m1, p2: m2},
@@ -231,7 +261,7 @@ def handle_restart(data):
     r["target"] = None
     emit("game_start", {"bat_first_sid": r["bat_first_sid"], "usernames": r["names"]}, room=room)
 
-# --- Run Server ---
+# --- Run ---
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
